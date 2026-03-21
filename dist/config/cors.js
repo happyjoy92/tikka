0d1ccb5e92604687e292c1a7a4d0ede11be1501c
happyjoy92 <happyjoy123987@gmail.com>
2026-03-21 05:18:49 -0700

    stuff

--- dist/config/cors.js
+++ dist/config/cors.js
@@ -2,13 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const corsOptions = {
     credentials: true,
-    origin: [
-        "http://localhost:5000",
-        "http://172.20.10.6:8080",
-        "http://10.15.245.81:8080",
-        "http://localhost:8080",
-        "https://app.local",
-    ],
+    origin: ["https://tikka-oa4x.onrender.com", "https://tikka.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
     exposedHeaders: ["Set-Cookie"],
